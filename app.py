d8df9c4d8849ad0fdaaf166d91b676821f2feadc
Madina <madinatussupova0820@gmail.com>
2023-10-26 09:13:14 +0600

    merge quantity and price cols, turn df into csv

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ data_files = ["./data/daily_sales_data_0.csv", "./data/daily_sales_data_1.csv", 
 data_mapper = map(lambda filename: pd.read_csv(filename, index_col=None, header=0), data_files)
 li = list(data_mapper)
 data = pd.concat(li, axis=0)
+# data = pd.read_csv("./data/daily_sales_data_0.csv")
 
-for index, row in data.iterrows():
-	pass
-    # if row["product"] != "pink morsel":
-    #     data.drop(index)
-        
+data.drop(data[data['product'] != "pink morsel"].index, inplace = True)
+
+data['sales'] = data['quantity'].astype(str) + data['price']
+data.drop(data.columns[[0, 1, 2]], axis=1, inplace=True)
+
+data.to_csv('pink_morsel_data.csv')
 print(data)
